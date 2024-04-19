08138003475e26dbf8ed3dcb30ae5a8b15d66971
jakeskehan11 <brojake87@gmail.com>
2024-04-19 15:17:56 +0800

    fix chart bugs

--- src/components/ui/chart.jsx
+++ src/components/ui/chart.jsx
@@ -1,10 +1,18 @@
 import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card";
 import { ResponsiveBar } from "@nivo/bar";
-import { ResponsiveLine } from "@nivo/line";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const Chart = () => {
   return (
-    <div className="flex justify-evenly items-center mt-10">
+    <div className="flex justify-evenly items-center mt-10 lg:mx-11">
       <Card className="w-5/12">
         <CardHeader>
           <CardTitle>Ticket Resolution Time</CardTitle>
@@ -24,17 +32,41 @@ const Chart = () => {
           </p>
         </CardHeader>
         <CardContent>
-          <LineChart className="aspect-[16/9]" />
+          <ResponsiveContainer className="aspect-[16/9]">
+            <AreaChart
+              width={500}
+              height={400}
+              data={LineChartdata}
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Area
+                type="monotone"
+                dataKey="ticket"
+                stroke="#003300"
+                fill="rgb(234 179 8)"
+              />
+            </AreaChart>
+          </ResponsiveContainer>
         </CardContent>
       </Card>
     </div>
   );
 };
+
 export default Chart;
 
-function BarChart(props) {
+function BarChart({ className }) {
   return (
-    <div {...props}>
+    <div className={className}>
       <ResponsiveBar
         data={[
           { name: "Jan", count: 111 },
@@ -50,102 +82,50 @@ function BarChart(props) {
         borderRadius={5}
         padding={0.5}
         colors={["#003300"]}
-        axisBottom={{
-          tickSize: 0,
-          tickPadding: 16,
-        }}
-        axisLeft={{
-          tickSize: 0,
-          tickValues: 4,
-          tickPadding: 16,
-        }}
+        axisBottom={{ tickSize: 0, tickPadding: 16 }}
+        axisLeft={{ tickSize: 0, tickValues: 4, tickPadding: 16 }}
         gridYValues={4}
         theme={{
           tooltip: {
-            chip: {
-              borderRadius: "9999px",
-            },
-            container: {
-              fontSize: "12px",
-              textTransform: "capitalize",
-            },
+            chip: { borderRadius: "9999px" },
+            container: { fontSize: "12px", textTransform: "capitalize" },
           },
-          grid: {
-            line: {
-              stroke: "#f3f4f6",
-            },
-          },
+          grid: { line: { stroke: "#f3f4f6" } },
         }}
         tooltipLabel={({ id }) => `${id}`}
         enableLabel={false}
-        role="application"
+        role="ticket"
         ariaLabel="A bar chart showing data"
       />
     </div>
   );
 }
 
-function LineChart(props) {
-  return (
-    <div {...props}>
-      <ResponsiveLine
-        data={[
-          {
-            id: "Ticket",
-            data: [
-              { x: "Jan", y: 500 },
-              { x: "Feb", y: 900 },
-              { x: "Mar", y: 2000 },
-              { x: "Apr", y: 400 },
-              { x: "May", y: 1500 },
-              { x: "Jun", y: 2600 },
-            ],
-          },
-          
-        ]}
-        margin={{ top: 10, right: 10, bottom: 40, left: 40 }}
-        xScale={{
-          type: "point",
-        }}
-        yScale={{
-          type: "linear",
-        }}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-          tickSize: 0,
-          tickPadding: 16,
-        }}
-        axisLeft={{
-          tickSize: 0,
-          tickValues: 5,
-          tickPadding: 16,
-        }}
-        colors={["#eab308"]}
-        pointSize={6}
-        useMesh={true}
-        gridYValues={6}
-        theme={{
-          tooltip: {
-            chip: {
-              borderRadius: "9999px",
-            },
-            container: {
-              fontSize: "12px",
-              textTransform: "capitalize",
-              borderRadius: "6px",
-            },
-          },
-          grid: {
-            line: {
-              stroke: "#f3f4f6",
-            },
-          },
-        }}
-        role="ticket"
-      />
-    </div>
-  );
-}
+const LineChartdata = [
+  {
+    name: "Jan",
+    ticket: 1500,
+  },
+  {
+    name: "Feb",
+    ticket: 800,
+  },
+  {
+    name: "March",
+    ticket: 2000,
+  },
+  {
+    name: "Apr",
+    ticket: 2500,
+  },
+  {
+    name: "May",
+    ticket: 3100,
+  },
+  {
+    name: "Jun",
+    ticket: 2600,
+  },
+];
 
-export { Chart, BarChart, LineChart };
+export { Chart, BarChart };
--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -6,56 +6,56 @@ const Dashboard = () => {
   return (
     <div className="ml-60 h-screen">
       <div className="flex justify-center items-start pt-20 gap-4 flex-wrap">
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between pb-2  space-y-0">
+        <Card className="xl:w-2/5 w-[22.6rem]">
+          <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0 lg">
             <CardTitle className="text-sm font-semibold">
               Total Tickets
             </CardTitle>
             <LuTicket className="w-4 h-4 text-gray-500 dark:text-gray-400" />
           </CardHeader>
-          <CardContent className="pr-52 pt-8">
+          <CardContent className="mt-8">
             <div className="text-2xl font-bold">2,600</div>
             <p className="text-xs text-gray-500 dark:text-gray-400">
               +12.1% from last month
             </p>
           </CardContent>
         </Card>
-        <Card>
+        <Card className="xl:w-2/5 w-[22.6rem]">
           <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
             <CardTitle className="text-sm font-semibold">
               Open Tickets
             </CardTitle>
             <LuTicket className="w-4 h-4 text-gray-500 dark:text-gray-400" />
           </CardHeader>
-          <CardContent className="pr-52 pt-8">
+          <CardContent className="mt-8">
             <div className="text-2xl font-bold">1,234</div>
             <p className="text-xs text-gray-500 dark:text-gray-400">
               +8.1% from last month
             </p>
           </CardContent>
         </Card>
-        <Card>
+        <Card className="xl:w-2/5 w-[22.6rem]">
           <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
             <CardTitle className="text-sm font-semibold">
               Pending Tickets
             </CardTitle>
             <LuTicket className="w-4 h-4 text-gray-500 dark:text-gray-400" />
           </CardHeader>
-          <CardContent className="pr-52 pt-8">
+          <CardContent className="mt-8">
             <div className="text-2xl font-bold">250</div>
             <p className="text-xs text-gray-500 dark:text-gray-400">
               +5% from last month
             </p>
           </CardContent>
         </Card>
-        <Card>
+        <Card className="xl:w-2/5 w-[22.6rem]">
           <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
             <CardTitle className="text-sm font-semibold">
               Closed Tickets
             </CardTitle>
             <LuTicket className="w-4 h-4 text-gray-500 dark:text-gray-400" />
           </CardHeader>
-          <CardContent className="pr-52 pt-8">
+          <CardContent className="mt-8">
             <div className="text-2xl font-bold">1,116</div>
             <p className="text-xs text-gray-500 dark:text-gray-400">
               +15% from last month
