b53b3aec6f189af31726a8ea9653480e06afe5ae
jakeskehan11 <159106035+jakeskehan11@users.noreply.github.com>
2024-05-21 20:19:22 +0800

    save api

--- client/src/hooks/useLogin.jsx
+++ client/src/hooks/useLogin.jsx
@@ -12,7 +12,7 @@ export const useLogin = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("/api/auth/login", {
+    const response = await fetch("https://tickita-api.vercel.app/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
