5cc7fcf61e1409d35670435bf2e588a148ee4e0e
jakeskehan11 <brojake87@gmail.com>
2024-05-23 02:19:03 +0800

    save

--- server/server.js
+++ server/server.js
@@ -12,7 +12,13 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://tickita.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
