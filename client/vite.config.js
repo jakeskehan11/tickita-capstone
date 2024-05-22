5571773558595035932f5f9eaa632bdf58a90b3b
jakeskehan11 <brojake87@gmail.com>
2024-05-23 05:19:46 +0800

    save

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://tickita-api.vercel.app",
+        target: "http://localhost:4000",
         changeOrigin: true,
       },
     },
