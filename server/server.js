df67b5731c8820885812d2544ae75d7e0d0a88b7
jakeskehan11 <brojake87@gmail.com>
2024-05-23 02:03:51 +0800

    save

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 const userRoutes = require("./routes/user");
 const jobTicketRoutes = require("./routes/jobTicket");
 const technicalJobTicketRoutes = require("./routes/technicalJobTicket");
@@ -10,10 +10,9 @@ const technicalJobTicketRoutes = require("./routes/technicalJobTicket");
 // express app
 const app = express();
 
-
-
 // middleware
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
